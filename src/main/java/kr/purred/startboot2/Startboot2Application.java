7fa3e1f7d2f5b54b430e6999deb9f4ae202a0bac
ThomasNam <purred@naver.com>
2018-11-08 15:34:56 +0900

    프로젝트 최초 커밋

---
+++ src/main/java/kr/purred/startboot2/Startboot2Application.java
@@ -0,0 +1,12 @@
+package kr.purred.startboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startboot2Application {
+
+	public static void main (String[] args) {
+		SpringApplication.run (Startboot2Application.class, args);
+	}
+}
